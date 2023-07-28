9bc4e2e9fd83bd1dcb4674a94014bfa14e9941fd
Lukewh <luke@lukewh.com>
2023-07-28 19:57:32 +0100

    Use requestUrl

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
-import { Plugin, Notice, Editor } from "obsidian";
+import { Plugin, Notice, Editor, requestUrl } from "obsidian";
 
 const insertName = (editor: Editor, name: string) => {
   const cursor = editor.getCursor();
   editor.replaceRange(name, cursor);
   editor.setCursor(cursor.line, cursor.ch + name.length);
 };
+
+const API_URL = "https://fantasyname.lukewh.com?family=t";
 
 export default class GMEmulator extends Plugin {
   async onload() {
@@ -13,9 +15,9 @@ export default class GMEmulator extends Plugin {
       id: "fantasy-name-insert",
       name: "Insert fantasy name",
       editorCallback: async (editor: Editor) => {
-        const resp = await fetch("https://fantasyname.lukewh.com?family=t");
-        const name = await resp.text();
-        new Notice(`Answer: ${name}`);
+        const resp = await requestUrl(API_URL);
+        const name = resp.text;
+        new Notice(`Name: ${name}`);
         insertName(editor, name);
       },
     });
@@ -24,11 +26,9 @@ export default class GMEmulator extends Plugin {
       id: "fantasy-name-insert-female",
       name: "Insert fantasy name (Female)",
       editorCallback: async (editor: Editor) => {
-        const resp = await fetch(
-          "https://fantasyname.lukewh.com?gender=f&family=t"
-        );
-        const name = await resp.text();
-        new Notice(`Answer: ${name}`);
+        const resp = await requestUrl(`${API_URL}&gender=f`);
+        const name = resp.text;
+        new Notice(`Name: ${name}`);
         insertName(editor, name);
       },
     });
@@ -37,11 +37,9 @@ export default class GMEmulator extends Plugin {
       id: "fantasy-name-insert-male",
       name: "Insert fantasy name (Male)",
       editorCallback: async (editor: Editor) => {
-        const resp = await fetch(
-          "https://fantasyname.lukewh.com?gender=m&family=t"
-        );
-        const name = await resp.text();
-        new Notice(`Answer: ${name}`);
+        const resp = await requestUrl(`${API_URL}&gender=m`);
+        const name = resp.text;
+        new Notice(`Name: ${name}`);
         insertName(editor, name);
       },
     });
